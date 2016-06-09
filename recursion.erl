9b8de39e905e07c65b49420b13955e92da1a6901
WarioR <vasiliy@sinyur.in>
2016-06-09 18:29:55 +0300

    complete recursion

--- recursion.erl
+++ recursion.erl
@@ -2,7 +2,7 @@
 -author("warior").
 
 %% API
--export([fac/1, len/1, tail_fac/1]).
+-export([fac/1, len/1, tail_fac/1, tail_len/1, tail_zip/2]).
 
 fac(N) when N == 0 -> 1;
 fac(N) when N > 0 -> N*fac(N-1).
@@ -15,3 +15,15 @@ tail_fac(N, Acc) -> tail_fac(N-1, Acc*N).
 len([]) -> 0;
 len([_|Sp]) -> 1+len(Sp);
 len(_) -> 1.
+
+tail_len(Sp) -> tail_len(Sp, 0).
+
+tail_len([], Acc) -> Acc;
+tail_len([_|Sp], Acc) -> tail_len(Sp, Acc+1).
+
+tail_zip(X,Y) -> tail_zip(X,Y,[]).
+
+tail_zip([],[], Acc) -> Acc;
+tail_zip(_, [], Acc) -> Acc;
+tail_zip([], _, Acc) -> Acc;
+tail_zip([Xx|X], [Yy|Y], Acc) -> lists:reverse(tail_zip(X, Y, [{Xx, Yy}| Acc])).
